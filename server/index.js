4bbce224f335f92e54aebe471918ec2ae2c2fbd3
Ramchandra Dalvi <rdalvi1210@gmail.com>
2025-07-08 22:46:21 +0530

    add .gitignore file

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
-import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-import connectDb from "./utils/db.js";
+import cors from "cors";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js"
-import postRoute from "./routes/post.route.js"
-import messageRoute from "./routes/message.route.js"
-import {app, server} from "./socket/socket.js"
-    
+import express from "express";
+import messageRoute from "./routes/message.route.js";
+import postRoute from "./routes/post.route.js";
+import userRoute from "./routes/user.route.js";
+import { app, server } from "./socket/socket.js";
+import connectDb from "./utils/db.js";
+
 dotenv.config({
-    path: ".env"
+  path: ".env",
 });
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middlewares
 app.use(express.json()); // Parse incoming JSON requests
@@ -21,16 +20,16 @@ app.use(cookieParser()); // Parse cookies
 app.use(express.urlencoded({ extended: true })); // Use express's built-in urlencoded
 
 const corsOptions = {
-    origin: "http://localhost:5173", 
-    credentials: true 
+  origin: "http://localhost:5173",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "I am coming from backend",
-        success: true
-    });
+  res.status(200).json({
+    message: "I am coming from backend",
+    success: true,
+  });
 });
 
 app.use("/api/v1/user", userRoute);
@@ -38,6 +37,6 @@ app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    connectDb();
+  console.log(`Server is listening on port ${PORT}`);
+  connectDb();
 });
